610eade9ffce575b15d6837ce33633843f678b86
Mason Batchelor <masonbatchelor81@gmail.com>
2025-07-15 10:34:52 -0500

    Minor update to CORS config

--- backend/src/main/java/com/example/inventory_service_demo/config/WebConfig.java
+++ backend/src/main/java/com/example/inventory_service_demo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.inventory_service_demo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
